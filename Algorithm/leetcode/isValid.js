09bbb2986e64723b1131f13f9fb8ec2639950c6f
huip <penghui1012@gmail.com>
2019-06-19 23:26:18 +0800

    Add: isValid && missingNumber

--- Algorithm/leetcode/isValid.js
+++ Algorithm/leetcode/isValid.js
@@ -2,6 +2,27 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+var isValid = function (s) {
+    let mapping = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+
+    let stack = []
+
+    for (let i = 0; i < s.length; i++) {
+        if (mapping[s[i]]) {
+            let topElement = stack.pop() || ''
+            if (topElement != mapping[s[i]]) {
+                return false
+            }
+        } else {
+            stack.push(s[i])
+        }
+    }
+
+    return stack.length === 0
 };
+
+console.log(isValid("([])"))
