a5a180a9d4379f92e2a42b69db0d7106cbc8760c
huip <penghui1012@gmail.com>
2019-11-28 20:46:31 +0800

    update

--- Code/LeetCode/187.find-repeated-dna-sequences.js
+++ Code/LeetCode/187.find-repeated-dna-sequences.js
@@ -10,13 +10,14 @@ var findRepeatedDnaSequences = function (s) {
     let res = []
     for (let i = 0; i < s.length - 9; i++) {
         let temp = s.substring(i, i + 10)
-        if (h1[temp]) {
-            res.push(temp)
-        } else {
-            h1[temp] = true
+        h1[temp] = h1[temp] ? h1[temp] + 1 : 1
+    }
+    for (let key in h1) {
+        if (h1[key] > 1) {
+            res.push(key)
         }
     }
     return res
 };
 
-console.log(findRepeatedDnaSequences('AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT'))
+console.log(findRepeatedDnaSequences('AAAAAAAAAAAAA'))
